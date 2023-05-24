b6c6b49828ab7b84862f47eb25ddf82dc1d83a35
pavelklm <pavel.klm.verstka@gmail.com>
2023-05-24 20:27:55 +0300

    create calendar rules

--- src/js/app.js
+++ src/js/app.js
@@ -2,89 +2,40 @@ import * as flsFunctions from './modules/functions.js';
 
 flsFunctions.isWebp();
 
-// Найти все элементы <select> в HTML, где будет отображаться список времени
 const selectElements = document.querySelectorAll('select');
-
-// Создать начальное время
-const startTime = new Date();
-startTime.setHours(9);
-startTime.setMinutes(0);
-
-// Создать конечное время
-const endTime = new Date();
-endTime.setHours(23);
-endTime.setMinutes(59);
-
-// Шаг времени в минутах
-const timeStep = 30;
-
-// Проход по каждому элементу <select>
-selectElements.forEach((selectElement) => {
-    // Очистить элемент перед добавлением новых вариантов времени
-    selectElement.innerHTML = '';
-
-    // Проход по времени с шагом 30 минут и добавление вариантов в текущий выпадающий список
-    for (
-        let currentTime = new Date(startTime);
-        currentTime <= endTime;
-        currentTime.setMinutes(currentTime.getMinutes() + timeStep)
-    ) {
-        // Получить часы и минуты из текущего времени
-        const hours = currentTime.getHours();
-        const minutes = currentTime.getMinutes();
-
-        // Преобразование часов и минут в строку в формате "чч:мм"
-        const timeString = `${('0' + hours).slice(-2)}:${('0' + minutes).slice(
-            -2
-        )}`;
-
-        // Создание нового элемента <option> и добавление его в текущий <select>
-        const optionElement = document.createElement('option');
-        optionElement.innerHTML = timeString;
-        selectElement.appendChild(optionElement);
-    }
-});
-
-// ...
-
 const date1Input = document.querySelector('.data1');
 const date2Input = document.querySelector('.data2');
 const form = document.querySelector('.calendar__form');
-
 const currentDate = new Date();
-
-// Установка минимальной даты в поле ввода date1Input
-// date1Input.min = currentDate.toISOString().slice(0, 10);
-
-// Установка текущей даты в поле ввода date1Input
-date1Input.value = currentDate.toISOString().slice(0, 10);
-
-// Создание элемента для вывода ошибки
 const errorMessage = document.createElement('span');
 errorMessage.classList.add('error-message');
 form.insertAdjacentElement('afterend', errorMessage);
 
-// ... ваш код ...
-// функция проверяет время равно между собой или что-то больше, а что-то меньше
+const startTime = new Date();
+startTime.setHours(9);
+startTime.setMinutes(0);
+
+const endTime = new Date();
+endTime.setHours(23);
+endTime.setMinutes(59);
+
+const timeStep = 30;
+
 function compareTime(time1, time2) {
     const [hours1, minutes1] = time1.split(':').map(Number);
     const [hours2, minutes2] = time2.split(':').map(Number);
-    if (hours1 > hours2) {
-        return -1;
-    } else if (hours1 < hours2) {
-        return 1;
-    } else {
-        if (minutes1 > minutes2) {
-            return -1;
-        } else if (minutes1 < minutes2) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+
+    return hours1 > hours2
+        ? -1
+        : hours1 < hours2
+        ? 1
+        : minutes1 > minutes2
+        ? -1
+        : minutes1 < minutes2
+        ? 1
+        : 0;
 }
 
-//если 2 даты равны между собой то функция проверяет время
 function checkDate(date1, date2) {
     if (date2.getTime() === date1.getTime()) {
         let time1 = selectElements[0].value;
@@ -98,6 +49,29 @@ function checkDate(date1, date2) {
     }
 }
 
+selectElements.forEach((selectElement) => {
+    selectElement.innerHTML = '';
+
+    for (
+        let currentTime = new Date(startTime);
+        currentTime <= endTime;
+        currentTime.setMinutes(currentTime.getMinutes() + timeStep)
+    ) {
+        const hours = currentTime.getHours();
+        const minutes = currentTime.getMinutes();
+
+        const timeString = `${('0' + hours).slice(-2)}:${('0' + minutes).slice(
+            -2
+        )}`;
+
+        const optionElement = document.createElement('option');
+        optionElement.innerHTML = timeString;
+        selectElement.appendChild(optionElement);
+    }
+});
+
+date1Input.value = currentDate.toISOString().slice(0, 10);
+
 const inputs = [date1Input, date2Input];
 inputs.forEach(function (input) {
     input.addEventListener('change', function () {
@@ -105,8 +79,7 @@ inputs.forEach(function (input) {
         let date2 = new Date(date2Input.value);
         let getDay = date1.getTime();
         let today = currentDate.getTime();
-        console.log(today);
-        console.log(getDay);
+
         if (getDay < today - 99999999) {
             errorMessage.textContent =
                 'дата подачи не может быть раньше чем сегодня';
@@ -124,7 +97,6 @@ inputs.forEach(function (input) {
     });
 });
 
-// Обработка изменений времени начала и конца аренды
 selectElements.forEach(function (selectElement) {
     selectElement.addEventListener('change', function () {
         let date1 = new Date(date1Input.value);
