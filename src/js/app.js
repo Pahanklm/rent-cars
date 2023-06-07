cb4c9bac2dbf9314cd19aea072697ac225015ceb
pavelklm <pavel.klm.verstka@gmail.com>
2023-06-07 20:19:32 +0300

    datapicker add

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,16 @@
 import * as flsFunctions from './modules/functions.js';
-
 flsFunctions.isWebp();
 
+import AirDatepicker from 'air-datepicker';
+
 const selectElements = document.querySelectorAll('select');
-const date1Input = document.querySelector('.data1');
-const date2Input = document.querySelector('.data2');
+const date1Input = document.querySelector('.calendar__input--date1');
+const date2Input = document.querySelector('.calendar__input--date2');
 const form = document.querySelector('.calendar__form');
 const currentDate = new Date();
 const errorMessage = document.createElement('span');
 errorMessage.classList.add('error-message');
 form.insertAdjacentElement('afterend', errorMessage);
-
 const startTime = new Date();
 startTime.setHours(9);
 startTime.setMinutes(0);
@@ -21,32 +21,28 @@ endTime.setMinutes(59);
 
 const timeStep = 30;
 
+let button = {
+    content: 'Сьогодні',
+    onClick: (dp) => {
+        let date = currentDate;
+        dp.selectDate(date);
+        dp.setViewDate(date);
+    },
+};
+
 function compareTime(time1, time2) {
     const [hours1, minutes1] = time1.split(':').map(Number);
     const [hours2, minutes2] = time2.split(':').map(Number);
 
-    return hours1 > hours2
-        ? -1
-        : hours1 < hours2
-        ? 1
-        : minutes1 > minutes2
-        ? -1
-        : minutes1 < minutes2
-        ? 1
-        : 0;
-}
-
-function checkDate(date1, date2) {
-    if (date2.getTime() === date1.getTime()) {
-        let time1 = selectElements[0].value;
-        let time2 = selectElements[1].value;
-        if (compareTime(time1, time2) < 1) {
-            errorMessage.textContent =
-                'Время конца аренды не может быть раньше или в то же время что и начало';
-        } else {
-            errorMessage.textContent = '';
-        }
+    if (hours1 !== hours2) {
+        return hours1 > hours2 ? -1 : 1;
     }
+
+    if (minutes1 !== minutes2) {
+        return minutes1 > minutes2 ? -1 : 1;
+    }
+
+    return 0;
 }
 
 selectElements.forEach((selectElement) => {
@@ -59,48 +55,187 @@ selectElements.forEach((selectElement) => {
     ) {
         const hours = currentTime.getHours();
         const minutes = currentTime.getMinutes();
-
         const timeString = `${('0' + hours).slice(-2)}:${('0' + minutes).slice(
             -2
         )}`;
 
         const optionElement = document.createElement('option');
-        optionElement.innerHTML = timeString;
+        optionElement.textContent = timeString;
         selectElement.appendChild(optionElement);
     }
 });
 
-date1Input.value = currentDate.toISOString().slice(0, 10);
-
-const inputs = [date1Input, date2Input];
-inputs.forEach(function (input) {
-    input.addEventListener('change', function () {
-        let date1 = new Date(date1Input.value);
-        let date2 = new Date(date2Input.value);
-        let getDay = date1.getTime();
-        let today = currentDate.getTime();
-
-        if (getDay < today - 99999999) {
-            errorMessage.textContent =
-                'дата подачи не может быть раньше чем сегодня';
-            date1Input.value = '';
-        } else {
-            errorMessage.textContent = '';
-        }
-
-        checkDate(date1, date2);
-        if (date1 > date2) {
-            errorMessage.textContent =
-                'дата подачи не может быть позже чем дата возврата';
-            date2Input.value = '';
-        }
+document.addEventListener('DOMContentLoaded', function () {
+    new AirDatepicker('#airdatepicker1', {
+        minDate: currentDate,
+        container: 'calendar',
+        disableNavWhenOutOfRange: true,
+        buttons: [button, 'clear'],
+        autoClose: true,
+        onSelect({ date, formattedDate, datepicker }) {
+            let date1 = airdatepicker1.value;
+            let date2 = airdatepicker2.value;
+            if (date1 > date2 && date1 !== '' && date2 !== '') {
+                errorMessage.textContent = 'lfksdjkfd';
+            } else {
+                errorMessage.textContent = '';
+            }
+            let time1 = selectElements[0].value;
+            let time2 = selectElements[1].value;
+            if (date1 === date2) {
+                if (date1 === '' || date2 === '') {
+                    errorMessage.textContent = '';
+                }
+                if (compareTime(time1, time2) < 1) {
+                    errorMessage.textContent =
+                        'Время конца аренды не может быть раньше или в то же время что и начало';
+                } else {
+                    errorMessage.textContent = '';
+                }
+                if (date1 !== date2) {
+                    errorMessage.textContent = '';
+                }
+            }
+        },
+        locale: {
+            days: [
+                'Неділя',
+                'Понеділок',
+                'Вівторок',
+                'Середа',
+                'Четвер',
+                'П’ятниця',
+                'Субота',
+            ],
+            daysShort: ['Нед', 'Пнд', 'Вів', 'Срд', 'Чтв', 'Птн', 'Сбт'],
+            daysMin: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            months: [
+                'Січень',
+                'Лютий',
+                'Березень',
+                'Квітень',
+                'Травень',
+                'Червень',
+                'Липень',
+                'Серпень',
+                'Вересень',
+                'Жовтень',
+                'Листопад',
+                'Грудень',
+            ],
+            monthsShort: [
+                'Січ',
+                'Лют',
+                'Бер',
+                'Кві',
+                'Тра',
+                'Чер',
+                'Лип',
+                'Сер',
+                'Вер',
+                'Жов',
+                'Лис',
+                'Гру',
+            ],
+            today: 'Сьогодні',
+            clear: 'Очистити',
+            dateFormat: 'dd.MM.yyyy',
+            timeFormat: 'HH:mm',
+            firstDay: 1,
+        },
+    });
+    new AirDatepicker('#airdatepicker2', {
+        minDate: currentDate,
+        disableNavWhenOutOfRange: true,
+        buttons: [button, 'clear'],
+        autoClose: true,
+        onSelect({ date, formattedDate, datepicker }) {
+            let date1 = airdatepicker1.value;
+            let date2 = airdatepicker2.value;
+            if (date1 > date2 && date1 !== '' && date2 !== '') {
+                errorMessage.textContent = 'lfksdjkfd';
+            } else {
+                errorMessage.textContent = '';
+            }
+            let time1 = selectElements[0].value;
+            let time2 = selectElements[1].value;
+            if (date1 === date2) {
+                if (date1 === '' || date2 === '') {
+                    errorMessage.textContent = '';
+                }
+                if (compareTime(time1, time2) < 1) {
+                    errorMessage.textContent =
+                        'Время конца аренды не может быть раньше или в то же время что и начало';
+                } else {
+                    errorMessage.textContent = '';
+                }
+                if (date1 !== date2) {
+                    errorMessage.textContent = '';
+                }
+            }
+        },
+        locale: {
+            days: [
+                'Неділя',
+                'Понеділок',
+                'Вівторок',
+                'Середа',
+                'Четвер',
+                'П’ятниця',
+                'Субота',
+            ],
+            daysShort: ['Нед', 'Пнд', 'Вів', 'Срд', 'Чтв', 'Птн', 'Сбт'],
+            daysMin: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            months: [
+                'Січень',
+                'Лютий',
+                'Березень',
+                'Квітень',
+                'Травень',
+                'Червень',
+                'Липень',
+                'Серпень',
+                'Вересень',
+                'Жовтень',
+                'Листопад',
+                'Грудень',
+            ],
+            monthsShort: [
+                'Січ',
+                'Лют',
+                'Бер',
+                'Кві',
+                'Тра',
+                'Чер',
+                'Лип',
+                'Сер',
+                'Вер',
+                'Жов',
+                'Лис',
+                'Гру',
+            ],
+            today: 'Сьогодні',
+            clear: 'Очистити',
+            dateFormat: 'dd.MM.yyyy',
+            timeFormat: 'HH:mm',
+            firstDay: 1,
+        },
     });
 });
 
 selectElements.forEach(function (selectElement) {
     selectElement.addEventListener('change', function () {
-        let date1 = new Date(date1Input.value);
-        let date2 = new Date(date2Input.value);
-        checkDate(date1, date2);
+        let date1 = airdatepicker1.value;
+        let date2 = airdatepicker2.value;
+        let time1 = selectElements[0].value;
+        let time2 = selectElements[1].value;
+        if (date1 === date2) {
+            if (compareTime(time1, time2) < 1) {
+                errorMessage.textContent =
+                    'Время конца аренды не может быть раньше или в то же время что и начало';
+            } else {
+                errorMessage.textContent = '';
+            }
+        }
     });
 });
